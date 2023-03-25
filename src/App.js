78454f17bcf6028324d98ab84deef06c7a164010
Kübra Çavuşoğlu <118998624+kubracvs@users.noreply.github.com>
2023-03-25 23:22:51 +0300

    v-1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 
 function App() {
+  const [veri, setVeri] = useState("");
+  const [tarih, setTarih] = useState("");
+
+  // setVeri(res.data[tarih])
+
+  useEffect(() => {
+
+    axios.get("https://deprem.piyanos.com/api")
+ 
+
+     .then(res=>setVeri(res.data[tarih]))
+    // .then(res => console.log(res))
+
+     .catch(err=>console.log(err))
+
+  },[veri,tarih])
+
+ 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-8 mx-auto mt-4">
+            <h2 className="text-center text-white diplay-3">
+              TÜRKİYE SON DEPREMLER RAPORU
+            </h2>
+            <input
+              type=""
+              
+              className="form-control"
+              onChange={(e)=>setTarih(e.target.value)}
+            />
+            {/* import Table from 'react-bootstrap/Table'; */}
+            <table className="table table-striped text-white">
+              <thead>
+                <tr>
+                  <th scope="col">#</th>
+                  <th scope="col">şehir</th>
+                  <th scope="col">şiddet</th>
+                  <th scope="col">derinlik</th>
+                
+                  
+                  
+                </tr>
+              </thead>
+              <tbody>
+                <tr  className="text-white">
+                 
+                  <th scope="row" className="text-white" >{veri===undefined ? "veri bekleniyor": veri.date}</th>
+                  <th scope="row" className="text-white" >{veri===undefined ? "veri bekleniyor": veri.region}</th>
+                  <th scope="row" className="text-white" >{veri===undefined? "veri bekleniyor": veri.intensity}</th>
+                  <th scope="row" className="text-white" >{veri===undefined? "veri bekleniyor": veri.depth}</th>
+                  
+                 
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
