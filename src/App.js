36a80e492dc7e5ff37b143884ead4b823983dcda
Kübra Çavuşoğlu <118998624+kubracvs@users.noreply.github.com>
2023-03-28 00:38:22 +0300

    v-1

--- src/App.js
+++ src/App.js
@@ -1,71 +1,12 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
 
+import ListItems from './components/ListItems'
 
-function App() {
-  const [veri, setVeri] = useState("");
-  const [tarih, setTarih] = useState("");
-
-  // setVeri(res.data[tarih])
-
-  useEffect(() => {
-
-    axios.get("https://deprem.piyanos.com/api")
- 
-
-     .then(res=>setVeri(res.data[tarih]))
-    // .then(res => console.log(res))
-
-     .catch(err=>console.log(err))
-
-  },[veri,tarih])
-
- 
-
+const App = () => {
   return (
-    <div className="App">
-      <div className="container">
-        <div className="row">
-          <div className="col-md-8 mx-auto mt-4">
-            <h2 className="text-center text-white diplay-3">
-              TÜRKİYE SON DEPREMLER RAPORU
-            </h2>
-            <input
-              type=""
-              
-              className="form-control"
-              onChange={(e)=>setTarih(e.target.value)}
-            />
-            {/* import Table from 'react-bootstrap/Table'; */}
-            <table className="table table-striped text-white">
-              <thead>
-                <tr>
-                  <th scope="col">#</th>
-                  <th scope="col">şehir</th>
-                  <th scope="col">şiddet</th>
-                  <th scope="col">derinlik</th>
-                
-                  
-                  
-                </tr>
-              </thead>
-              <tbody>
-                <tr  className="text-white">
-                 
-                  <th scope="row" className="text-white" >{veri===undefined ? "veri bekleniyor": veri.date}</th>
-                  <th scope="row" className="text-white" >{veri===undefined ? "veri bekleniyor": veri.region}</th>
-                  <th scope="row" className="text-white" >{veri===undefined? "veri bekleniyor": veri.intensity}</th>
-                  <th scope="row" className="text-white" >{veri===undefined? "veri bekleniyor": veri.depth}</th>
-                  
-                 
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        </div>
-      </div>
+    <div>
+      <ListItems/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/ListItems.jsx
@@ -0,0 +1,95 @@
+// import axios from "axios";
+import React, { useEffect, useState } from "react";
+
+const ListItems = () => {
+  const [veri, setVeri] = useState([]);
+  const [tarih, setTarih] = useState("");
+  const BASE_URL = "https://deprem.piyanos.com/api";
+
+ 
+  
+
+  //   const liste = () => {
+  //     <li>
+  //       <p className="text-white">
+  //         {veri === undefined ? "veri bekleniyor" : veri.date}
+  //       </p>
+  //       <p className="text-white">
+  //         {veri === undefined ? "veri bekleniyor" : veri.region}
+  //       </p>
+  //       <p className="text-white">
+  //         {veri === undefined ? "veri bekleniyor" : veri.intensity}
+  //       </p>
+  //       <p className="text-white">
+  //         {veri === undefined ? "veri bekleniyor" : veri.depth}
+  //       </p>
+  //     </li>;
+  //   };
+  useEffect(() => {
+    
+    fetch(`${BASE_URL}?.date=${tarih}`)
+      .then((res) => res.json())
+      .then((res) => setVeri(res))
+      .then((res) => console.log(res))
+
+      .catch((err) => console.log(err));
+  }, [veri,tarih]);
+  const listem = veri.map((item) => (
+    <tr className="text-white">
+      <th scope="row" className="text-white">
+        {item === undefined ? "item bekleniyor" : "#" }
+      </th>
+      <th scope="row" className="text-white">
+        {item === undefined ? "item bekleniyor" : item.date}
+      </th>
+      <th scope="row" className="text-white">
+        {item === undefined ? "item bekleniyor" : item.time}
+      </th>
+      <th scope="row" className="text-white">
+        {item === undefined ? "item bekleniyor" : item.region}
+      </th>
+      <th scope="row" className="text-white">
+        {item === undefined ? "item bekleniyor" : item.intensity}
+      </th>
+      <th scope="row" className="text-white">
+        {item === undefined ? "item bekleniyor" : item.depth}
+      </th>
+    </tr>
+  ));
+
+  return (
+    <div className="list">
+      <div className="container">
+        <div>
+          <div calssName="md-8 mx-auto mt-4">
+            <h2 className="text-center text-white diplay-3">
+              TÜRKİYE SON DEPREMLER RAPORU
+            </h2>
+            <input
+              type="date"
+              className="form-control"
+              onChange={(e) => setTarih(e.target.value)}
+            />
+            <table className="table table-striped text-white">
+              <thead>
+                <tr>
+                  <th scope="col">#</th>
+                  <th scope="col">Tarih</th>
+                  <th scope="col">Saat</th>
+                  <th scope="col">Konum</th>
+                  <th scope="col">Şiddet</th>
+                  <th scope="col">Derinlik</th>
+                </tr>
+              </thead>
+              <tbody>
+               {listem}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ListItems;
